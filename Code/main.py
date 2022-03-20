875e3f6f8629bec06a06d4130bc428d30aa72ce2
Tom` <b9305785@ncl.ac.uk>
2022-03-20 11:00:57 +0000

    Parameters stored in JSON

--- Code/main.py
+++ Code/main.py
@@ -1,31 +1,19 @@
 from Code.clustering import kmeans_clustering, hierarchical_clustering, density_clustering
 from Code.data import diff_calc, prepare_data
 from Code.ranking import param_sweep
+import json
 
-# Parameters for algorithms
-kmeans_params = {
-    "n_clusters": 10,
-    "n_init": 10,  # Number of different centroid seeds to use
-    "max_iter": 300  # Maximum number of iterations for one run
-}
-hierarchical_params = {
-    "n_clusters": 10,
-    "linkage": "ward",  # Decides which points distances are measured between
-    "affinity": "euclidean"  # How distance between points is measured, must euclidean if using ward
-}
-density_params = {
-    "eps": 0.5,  # Decides the max distance between points in one neighbourhood
-    "min_samples": 5,  # Decides what can be a core point
-    "p": 2,  # How distance will be calculated between points
-    "min_cluster_size": None
-}
+# Default parameters for the algorithms
+f = open("../config/def_params.json")
+params = json.load(f)
+f.close()
 
 
 # Example function calls
 def function_test(diff):
-    param_sweep("K-Means", kmeans_params, diff[0], "n_clusters", [4, 5, 6])
-    kmeans_clustering(diff[0], kmeans_params, "print")
-    # kmeans_clustering(diff[0], k_means_params, "plot")
+    param_sweep("K-Means", params['kmeans'], diff[0], "n_clusters", [4, 5, 6])
+    kmeans_clustering(diff[0], params['kmeans'], "print")
+    # kmeans_clustering(diff[0], params['kmeans'], "plot")
 
 
 # Contains a list of dataframes showing the differences of attributes for left and right index knees
